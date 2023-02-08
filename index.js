5eacdb10bfe88288f3fc80b77202b3ba206665b6
ChristinaGraham <cgraham74@gmail.com>
2023-02-08 10:36:12 -0600

    Added player name prompt, refactored functions and variable names to separate dealer from player

--- index.js
+++ index.js
@@ -3,27 +3,50 @@ const player = {
   chips: 0,
 };
 
+player.name = prompt('What is your name?');
+
 const cardType = ["Club", "Diamond", "Spade", "Heart"];
 const faceCard = ["King", "Queen", "Jack"];
-let playerCards = [];
-let dealerCards = [];
-let cards = [];
-let sum;
-let hasBlackJack = false;
-let isAlive = false;
-let isHold = false;
-let message = "";
+
+const startGameEl = document.getElementById("start-game-el");
 const messageEl = document.getElementById("message-el");
-const sumEl = document.getElementById("sum-el");
+const playerSumEl = document.getElementById("player-sum-el");
 const cardsEl = document.getElementById("cards-el");
 const playerEl = document.getElementById("player-el");
 const playerCardsDisplayEl = document.querySelector(".player-card-container");
 const dealerDisplayEl = document.querySelector(".dealer-card-container");
 const playerHold = document.getElementById("player-hold-btn");
+const newCardEl = document.getElementById("newcard-el");
+
+let playerCards = [];
+let dealerCards = [];
+let cards = [];
+let playerSum;
+let dealerSum;
+let hasBlackJack = false;
+let isAlive = false;
+let isHold = false;
+let message = "";
 let cardImage;
 
-
 playerEl.textContent = player.name + ": $" + player.chips;
+
+
+startGameEl.addEventListener("click", function(){
+ isAlive = true;
+  player.chips -= betMoney();
+  playerEl.textContent = player.name + ": $" + player.chips;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  playerSum = firstCard[0] + secondCard[0];
+
+  let firstDealerCard = getRandomCard();
+  dealerCards = [firstDealerCard];
+  dealerSum = dealerCards[0];
+  renderGame();
+
+})
 
 function getPlayerName(){
 // Create input box to get players Name
@@ -36,15 +59,24 @@ function betMoney(){
   return 5;
 }
 
+/**
+ * A button click event that will allow
+ * a player to hold if they do not want another card
+ */
 playerHold.addEventListener("click", function(){
   isHold = true
   console.log({isHold})
   return isHold
-//Allow player to hold if they do not want anymore cards
-// and then pass the turn to the dealer
 })
 
-
+/**
+ * Generates a random number between 1 and 13
+ * Assigns card images to the value of the generated number
+ * Aces are assigned value of 11 if the generated value is 1
+ * Face cards are assigned the value of 10 for any generated number 11 - 13
+ * @returns An Array containing the random number to use for score keeing 
+ * and the assigned card image
+ */
 function getRandomCard() {
   let randomNum = Math.floor(Math.random() * 13) + 1;
   if (randomNum === 1) {
@@ -59,29 +91,9 @@ function getRandomCard() {
     return [10, cardImage];
   } else {
     cardImage = randomNum + " " + cardType[Math.floor(Math.random() * 4)];
-
     return [randomNum, cardImage];
   }
 }
-
-
-
-function startGame() {
-  isAlive = true;
-  player.chips -= betMoney();
-  playerEl.textContent = player.name + ": $" + player.chips;
-  let firstCard = getRandomCard();
-  let secondCard = getRandomCard();
-
-  let firstDealerCard = getRandomCard();
-
-  dealerCards = [firstDealerCard];
-  cards = [firstCard, secondCard];
-  sum = firstCard[0] + secondCard[0];
-  renderGame();
-}
-
-
 
 function displayCards(){
   playerCardsDisplayEl.innerHTML = ""; 
@@ -99,14 +111,14 @@ function displayCards(){
 }
 
 function renderGame() {
-  cardsEl.textContent = "Cards: ";
+  cardsEl.textContent = "Your cards: ";
   displayCards();
-  sumEl.textContent = "Points: " + sum;
+  playerSumEl.textContent = "Points: " + playerSum;
     message = "Do you want to draw another card?";
-  if (sum > 21) {
+  if (playerSum > 21) {
     message = "Busted!";
     isAlive = false;
-  } else if (sum === 21) {
+  } else if (playerSum === 21) {
     message = "BlackJack!";
     player.chips += betMoney() * 2;
     playerEl.textContent = player.name + ": $" + player.chips;
@@ -123,17 +135,28 @@ function renderGame() {
 //Maybe take in a parameter ?  renderGame(opponent)  opponent = player?  Dealer?  If player - do the player stuff.  if dealer
 //then score for dealer and dealer messages. 
 
-function newCard() {
-  let newCard = getRandomCard();
+newCardEl.addEventListener("click", function(){
+   let newCard = getRandomCard();
   if (isAlive && !hasBlackJack && !isHold) {
-    sum += newCard[0];
+    playerSum += newCard[0];
     cards.push(newCard);
     renderGame();
   }
   if (isAlive && isHold){
+    //if is alive and ishold - dealers turn
     dealerCards.push(newCard);
     renderGame();
   }
+});
+
+
+function dealerTurn(){
+//Deal cards to dealer until the dealers score is above player's score or at 21. 
+//if dealers score above 21 - player wins.
+//if dealers score is 21 - dealer wins.
+//if dealers score is above players score and less than 21 - Dealer wins
+
+//render game
 }
 
 function randomNum(min, max){
